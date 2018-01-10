f500333e4a005f5e474a09485c5c10a5194ab187
suwigyarathore <Suwi.Rathore@klm.com>
2018-01-10 14:00:29 +0100

    Submitting form, resetting values and showing submitted form in DOM

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,37 @@ import { NgForm } from '@angular/forms'
 export class AppComponent {
   @ViewChild('f') signupForm: NgForm;
   defaultQuestion = 'pet';
-  answer = ""
+  answer = "";
+  genders = ['male', 'female'];
+  user = {
+    userName: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  submitted = false;
 
   suggestUserName () {
-    const suggestedName = 'Superuser';
+    const suggestedName = 'Superuser'
+
+    //For all values
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // });
+
+    //For patching values
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
   }
 
   // onSubmit (form: NgForm) {
@@ -20,6 +47,13 @@ export class AppComponent {
   // }
 
   onSubmit (form: NgForm) {
-    console.log(this.signupForm);
+    this.submitted = true;
+    this.user.userName = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
+
+    this.signupForm.reset();
   }
 }
